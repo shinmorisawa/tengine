9b444b9a2ac0c67dbe73938a7c65cf5a57c321e3
Shin Morisawa <git@tryh4rd.dev>
2026-04-26 19:57:49 +0900

    ok

--- include/color.h
+++ include/color.h
@@ -12,7 +12,7 @@
 #define MAX_HANDLERS 4
 
 typedef struct ColorRGB {
-    double r, g, b; 
+    double r, g, b;
 } ColorRGB; // linear sRGB, D65 (yes i did my research)
 
 typedef struct ColorLAB {
@@ -66,6 +66,10 @@ typedef struct Color {
 
     ColorSpaces type;
 } Color;
+
+typedef struct CMFEntry {
+    double x, y, z;
+} CMFEntry;
 
 // beware:
 // t is assumed to be between 0,1
@@ -149,52 +153,64 @@ ColorOKLAB color_oklab_lerp(ColorOKLAB start, ColorOKLAB end, double t);
 // Linear interpolate between 2 OKLch colors
 ColorOKLCH color_oklch_lerp(ColorOKLCH start, ColorOKLCH end, double t);
 
+// install a ColorHandler
 void color_install_handler(ColorHandler handler);
+// run a ColorHandler
 void color_run_handler(u64 index, Color color);
 
+// convert linear rgb to sRGB
 ColorRGB linear_to_srgb(ColorRGB rgb);
 
+// sample the cmf lut from cie 1931 2deg 1nm
+ColorXYZ color_sample_cmf(double wavelength);
+
+// does something with blackbodies, idk
+double color_planck_spectral_exitance(double wavelength, double kelvin);
+
+// convert kelvin to xyz
+ColorXYZ color_kelvin_to_xyz(double kelvin);
+// convert refractive index to xyz (cherenkov radiation)
+ColorXYZ color_cherenkov_to_xyz(double refractive_index);
+
+// tonemapping
+ColorRGB color_tonemap_lottes(ColorRGB rgb);
+
 #ifndef NO_COLOR_IMPLEMENTATION
-#include <stdio.h>
-#include <math.h>
+#    include <math.h>
+#    include <stdio.h>
+
+#    define PI             3.14159265358979323846
+#    define PLANCK_H       6.62607015e-34
+#    define SPEED_OF_LIGHT 299792458.0
+#    define BOLTZMANN_K    1.380649e-23
 
 // matrices and stuff
-static double xyz_rgb_mat[3][3] = {
-    { 3.2406255, -1.5372080, -0.4986286 },
-    { -0.9689307, 1.8757561, 0.0415175 },
-    { 0.0557101, -0.2040211, 1.0569959 }
-};
+static double xyz_rgb_mat[3][3] = {{3.2406255, -1.5372080, -0.4986286},
+                                   {-0.9689307, 1.8757561, 0.0415175},
+                                   {0.0557101, -0.2040211, 1.0569959}};
 
 static double xyz_oklab_mat1[3][3] = {
-    { 0.8189330101, 0.3618667424, -0.1288597137 },
-    { 0.0329845436, 0.9293118715, 0.0361456387 },
-    { 0.0482003018, 0.2643662691, 0.6338517070 }
-};
+    {0.8189330101, 0.3618667424, -0.1288597137},
+    {0.0329845436, 0.9293118715, 0.0361456387},
+    {0.0482003018, 0.2643662691, 0.6338517070}};
 
 static double xyz_oklab_mat2[3][3] = {
-    { 0.2104542553, 0.7836177850, -0.0040720468 },
-    { 1.9779984951, -2.4285922050, 0.4505937099 },
-    { 0.0259040371, 0.7827717662, -0.8086757660 }
-};
+    {0.2104542553, 0.7836177850, -0.0040720468},
+    {1.9779984951, -2.4285922050, 0.4505937099},
+    {0.0259040371, 0.7827717662, -0.8086757660}};
 
 static double xyz_oklab_mat1_i[3][3] = {
-    { 1.2270138511035211, -0.5577999806518222, 0.2812561489664678 },
-    { -0.0405801784232806, 1.11225686961683, -0.0716766786656012 },
-    { -0.0763812845057069, -0.4214819784180126, 1.5861632204407947 }
-};
+    {1.2270138511035211, -0.5577999806518222, 0.2812561489664678},
+    {-0.0405801784232806, 1.11225686961683, -0.0716766786656012},
+    {-0.0763812845057069, -0.4214819784180126, 1.5861632204407947}};
 
 static double xyz_oklab_mat2_i[3][3] = {
-    { 1.0101010086264997, 0.39527151599023685, 0.21515876639733963 },
-    { 1.0101010191631066, -0.10662761851830986, -0.06449916644185308 },
-    { 1.0101010654162885, -0.09055045833844474, -1.2921305295637735 }
-};
+    {1.0101010086264997, 0.39527151599023685, 0.21515876639733963},
+    {1.0101010191631066, -0.10662761851830986, -0.06449916644185308},
+    {1.0101010654162885, -0.09055045833844474, -1.2921305295637735}};
 
 static double lab_delta = 6.0 / 29.0;
-static double lab_d65[3] = {
-    95.0489 / 100.0,
-    100.0 / 100.0,
-    108.8840 / 100.0
-};
+static double lab_d65[3] = {95.0489 / 100.0, 100.0 / 100.0, 108.8840 / 100.0};
 
 // handlers
 static ColorHandler handlers[MAX_HANDLERS] = {0};
@@ -202,23 +218,19 @@ static u64 handler_count = 0;
 
 ColorRGB color_xyz_to_rgb(ColorXYZ xyz) {
     ColorRGB rgb = {0};
-    rgb.r = xyz_rgb_mat[0][0] * xyz.x + xyz_rgb_mat[0][1] * xyz.y + xyz_rgb_mat[0][2] * xyz.z;
-    rgb.g = xyz_rgb_mat[1][0] * xyz.x + xyz_rgb_mat[1][1] * xyz.y + xyz_rgb_mat[1][2] * xyz.z;
-    rgb.b = xyz_rgb_mat[2][0] * xyz.x + xyz_rgb_mat[2][1] * xyz.y + xyz_rgb_mat[2][2] * xyz.z;
+    rgb.r = xyz_rgb_mat[0][0] * xyz.x + xyz_rgb_mat[0][1] * xyz.y +
+            xyz_rgb_mat[0][2] * xyz.z;
+    rgb.g = xyz_rgb_mat[1][0] * xyz.x + xyz_rgb_mat[1][1] * xyz.y +
+            xyz_rgb_mat[1][2] * xyz.z;
+    rgb.b = xyz_rgb_mat[2][0] * xyz.x + xyz_rgb_mat[2][1] * xyz.y +
+            xyz_rgb_mat[2][2] * xyz.z;
 
-    if (rgb.r > 1)
-        rgb.r = 1;
-    if (rgb.g > 1)
-        rgb.g = 1;
-    if (rgb.b > 1)
-        rgb.b = 1;
-
-    if (rgb.r < 0)
-        rgb.r = 0;
-    if (rgb.g < 0)
-        rgb.g = 0;
-    if (rgb.b < 0)
-        rgb.b = 0;
+    if (rgb.r > 1.0) rgb.r = 1.0;
+    if (rgb.r < 0.0) rgb.r = 0.0;
+    if (rgb.g > 1.0) rgb.g = 1.0;
+    if (rgb.g < 0.0) rgb.g = 0.0;
+    if (rgb.b > 1.0) rgb.b = 1.0;
+    if (rgb.b < 0.0) rgb.b = 0.0;
 
     return rgb;
 }
@@ -253,15 +265,17 @@ static double lab_xyz_conv_helper(double t) {
     if (t > (lab_delta * lab_delta * lab_delta)) {
         return cbrt(t);
     } else {
-        return (1.0/3.0 * t * pow(lab_delta, -2)) + (4.0/29.0);
+        return (1.0 / 3.0 * t * pow(lab_delta, -2)) + (4.0 / 29.0);
     }
 }
 
 ColorLAB color_xyz_to_lab(ColorXYZ xyz) {
     ColorLAB lab = {0};
     lab.l = 116.0 * lab_xyz_conv_helper(xyz.y / lab_d65[1]) - 16.0;
-    lab.a = 500.0 * (lab_xyz_conv_helper(xyz.x / lab_d65[0]) - lab_xyz_conv_helper(xyz.y / lab_d65[1]));
-    lab.b = 200.0 * (lab_xyz_conv_helper(xyz.y / lab_d65[1]) - lab_xyz_conv_helper(xyz.z / lab_d65[2]));
+    lab.a = 500.0 * (lab_xyz_conv_helper(xyz.x / lab_d65[0]) -
+                     lab_xyz_conv_helper(xyz.y / lab_d65[1]));
+    lab.b = 200.0 * (lab_xyz_conv_helper(xyz.y / lab_d65[1]) -
+                     lab_xyz_conv_helper(xyz.z / lab_d65[2]));
     lab.l /= 100.0;
     lab.a = (lab.a + 128.0) / 255.0;
     lab.b = (lab.b + 128.0) / 255.0;
@@ -297,7 +311,7 @@ ColorLAB color_oklch_to_lab(ColorOKLCH oklch) {
 ColorLCH color_xyz_to_lch(ColorXYZ xyz) {
     ColorLAB lab = color_xyz_to_lab(xyz);
     ColorLCH lch = color_lab_to_lch(lab);
-    return lch; 
+    return lch;
 }
 
 ColorLCH color_lab_to_lch(ColorLAB lab) {
@@ -318,30 +332,36 @@ ColorLCH color_lab_to_lch(ColorLAB lab) {
 ColorLCH color_oklab_to_lch(ColorOKLAB oklab) {
     ColorLAB lab = color_oklab_to_lab(oklab);
     ColorLCH lch = color_lab_to_lch(lab);
-    return lch; 
+    return lch;
 }
 
 ColorLCH color_oklch_to_lch(ColorOKLCH oklch) {
     ColorLAB lab = color_oklch_to_lab(oklch);
     ColorLCH lch = color_lab_to_lch(lab);
-    return lch; 
+    return lch;
 }
 
 ColorOKLAB color_xyz_to_oklab(ColorXYZ xyz) {
     ColorOKLAB oklab = {0};
     ColorOKLAB temp = {0};
     // apply the linear map into an LMS-like color space
-    temp.l = xyz_oklab_mat1[0][0] * xyz.x + xyz_oklab_mat1[0][1] * xyz.y + xyz_oklab_mat1[0][2] * xyz.z;
-    temp.a = xyz_oklab_mat1[1][0] * xyz.x + xyz_oklab_mat1[1][1] * xyz.y + xyz_oklab_mat1[1][2] * xyz.z;
-    temp.b = xyz_oklab_mat1[2][0] * xyz.x + xyz_oklab_mat1[2][1] * xyz.y + xyz_oklab_mat1[2][2] * xyz.z;
+    temp.l = xyz_oklab_mat1[0][0] * xyz.x + xyz_oklab_mat1[0][1] * xyz.y +
+             xyz_oklab_mat1[0][2] * xyz.z;
+    temp.a = xyz_oklab_mat1[1][0] * xyz.x + xyz_oklab_mat1[1][1] * xyz.y +
+             xyz_oklab_mat1[1][2] * xyz.z;
+    temp.b = xyz_oklab_mat1[2][0] * xyz.x + xyz_oklab_mat1[2][1] * xyz.y +
+             xyz_oklab_mat1[2][2] * xyz.z;
     // cuberoot non-linearity? idk man
     temp.l = cbrt(temp.l);
     temp.a = cbrt(temp.a);
     temp.b = cbrt(temp.b);
     // now apply ANOTHER LINEAR MAP???
-    oklab.l = xyz_oklab_mat2[0][0] * temp.l + xyz_oklab_mat2[0][1] * temp.a + xyz_oklab_mat2[0][2] * temp.b;
-    oklab.a = xyz_oklab_mat2[1][0] * temp.l + xyz_oklab_mat2[1][1] * temp.a + xyz_oklab_mat2[1][2] * temp.b;
-    oklab.b = xyz_oklab_mat2[2][0] * temp.l + xyz_oklab_mat2[2][1] * temp.a + xyz_oklab_mat2[2][2] * temp.b;
+    oklab.l = xyz_oklab_mat2[0][0] * temp.l + xyz_oklab_mat2[0][1] * temp.a +
+              xyz_oklab_mat2[0][2] * temp.b;
+    oklab.a = xyz_oklab_mat2[1][0] * temp.l + xyz_oklab_mat2[1][1] * temp.a +
+              xyz_oklab_mat2[1][2] * temp.b;
+    oklab.b = xyz_oklab_mat2[2][0] * temp.l + xyz_oklab_mat2[2][1] * temp.a +
+              xyz_oklab_mat2[2][2] * temp.b;
     return oklab;
 }
 
@@ -408,7 +428,7 @@ static double xyz_lab_conv_helper(double t) {
     if (t > lab_delta) {
         return t * t * t;
     } else {
-        return 3 * (lab_delta * lab_delta) * (t - (4.0/29.0));
+        return 3 * (lab_delta * lab_delta) * (t - (4.0 / 29.0));
     }
 }
 
@@ -417,9 +437,11 @@ ColorXYZ color_lab_to_xyz(ColorLAB lab) {
     lab.l *= 100.0;
     lab.a = lab.a * 255.0 - 128.0;
     lab.b = lab.b * 255.0 - 128.0;
-    xyz.x = lab_d65[0] * xyz_lab_conv_helper((lab.l + 16.0) / 116.0 + (lab.a / 500.0));
+    xyz.x = lab_d65[0] *
+            xyz_lab_conv_helper((lab.l + 16.0) / 116.0 + (lab.a / 500.0));
     xyz.y = lab_d65[1] * xyz_lab_conv_helper((lab.l + 16.0) / 116.0);
-    xyz.z = lab_d65[2] * xyz_lab_conv_helper((lab.l + 16.0) / 116.0 - (lab.b / 200.0));
+    xyz.z = lab_d65[2] *
+            xyz_lab_conv_helper((lab.l + 16.0) / 116.0 - (lab.b / 200.0));
     return xyz;
 }
 
@@ -434,18 +456,27 @@ ColorXYZ color_oklab_to_xyz(ColorOKLAB oklab) {
     ColorOKLAB temp = {0};
 
     // multiply by inverse mat2
-    temp.l = xyz_oklab_mat2_i[0][0] * oklab.l + xyz_oklab_mat2_i[0][1] * oklab.a + xyz_oklab_mat2_i[0][2] * oklab.b;
-    temp.a = xyz_oklab_mat2_i[1][0] * oklab.l + xyz_oklab_mat2_i[1][1] * oklab.a + xyz_oklab_mat2_i[1][2] * oklab.b;
-    temp.b = xyz_oklab_mat2_i[2][0] * oklab.l + xyz_oklab_mat2_i[2][1] * oklab.a + xyz_oklab_mat2_i[2][2] * oklab.b;
+    temp.l = xyz_oklab_mat2_i[0][0] * oklab.l +
+             xyz_oklab_mat2_i[0][1] * oklab.a +
+             xyz_oklab_mat2_i[0][2] * oklab.b;
+    temp.a = xyz_oklab_mat2_i[1][0] * oklab.l +
+             xyz_oklab_mat2_i[1][1] * oklab.a +
+             xyz_oklab_mat2_i[1][2] * oklab.b;
+    temp.b = xyz_oklab_mat2_i[2][0] * oklab.l +
+             xyz_oklab_mat2_i[2][1] * oklab.a +
+             xyz_oklab_mat2_i[2][2] * oklab.b;
 
     // cube to undo the cuberoot
     temp.l = pow(temp.l, 3.0);
     temp.a = pow(temp.a, 3.0);
     temp.b = pow(temp.b, 3.0);
 
-    xyz.x = xyz_oklab_mat1_i[0][0] * temp.l + xyz_oklab_mat1_i[0][1] * temp.a + xyz_oklab_mat1_i[0][2] * temp.b;
-    xyz.y = xyz_oklab_mat1_i[1][0] * temp.l + xyz_oklab_mat1_i[1][1] * temp.a + xyz_oklab_mat1_i[1][2] * temp.b;
-    xyz.z = xyz_oklab_mat1_i[2][0] * temp.l + xyz_oklab_mat1_i[2][1] * temp.a + xyz_oklab_mat1_i[2][2] * temp.b;
+    xyz.x = xyz_oklab_mat1_i[0][0] * temp.l + xyz_oklab_mat1_i[0][1] * temp.a +
+            xyz_oklab_mat1_i[0][2] * temp.b;
+    xyz.y = xyz_oklab_mat1_i[1][0] * temp.l + xyz_oklab_mat1_i[1][1] * temp.a +
+            xyz_oklab_mat1_i[1][2] * temp.b;
+    xyz.z = xyz_oklab_mat1_i[2][0] * temp.l + xyz_oklab_mat1_i[2][1] * temp.a +
+            xyz_oklab_mat1_i[2][2] * temp.b;
 
     return xyz;
 }
@@ -506,42 +537,34 @@ void color_run_handler(u64 index, Color color) {
         printf("[color.h] handler index too high, specify lower index\n");
     }
 
+    if (index > handler_count) {
+        printf("[color.h] handler index does not exist\n");
+    }
+
     ColorHandler handler = handlers[index];
 
     if ((handler.supported_spaces & RGB) != 0) {
-        if (color.type == RGB) {
-            handler.color_rgb(color.color_rgb);
-        }
+        if (color.type == RGB) { handler.color_rgb(color.color_rgb); }
     }
 
     if ((handler.supported_spaces & LAB) != 0) {
-        if (color.type == LAB) {
-            handler.color_lab(color.color_lab);
-        }
+        if (color.type == LAB) { handler.color_lab(color.color_lab); }
     }
 
     if ((handler.supported_spaces & LCH) != 0) {
-        if (color.type == LCH) {
-            handler.color_lch(color.color_lch);
-        }
+        if (color.type == LCH) { handler.color_lch(color.color_lch); }
     }
 
     if ((handler.supported_spaces & OKLAB) != 0) {
-        if (color.type == OKLAB) {
-            handler.color_oklab(color.color_oklab);
-        }
+        if (color.type == OKLAB) { handler.color_oklab(color.color_oklab); }
     }
 
     if ((handler.supported_spaces & OKLCH) != 0) {
-        if (color.type == OKLCH) {
-            handler.color_oklch(color.color_oklch);
-        }
+        if (color.type == OKLCH) { handler.color_oklch(color.color_oklch); }
     }
 
     if ((handler.supported_spaces & XYZ) != 0) {
-        if (color.type == XYZ) {
-            handler.color_xyz(color.color_xyz);
-        }
+        if (color.type == XYZ) { handler.color_xyz(color.color_xyz); }
     }
 }
 
@@ -549,26 +572,125 @@ ColorRGB linear_to_srgb(ColorRGB rgb) {
     if (rgb.r <= 0.0031308) {
         rgb.r *= 12.92;
     } else {
-        rgb.r = 1.055 * pow(rgb.r, 1.0/2.4) - 0.055;
+        rgb.r = 1.055 * pow(rgb.r, 1.0 / 2.4) - 0.055;
     }
 
     if (rgb.g <= 0.0031308) {
         rgb.g *= 12.92;
     } else {
-        rgb.g = 1.055 * pow(rgb.g, 1.0/2.4) - 0.055;
+        rgb.g = 1.055 * pow(rgb.g, 1.0 / 2.4) - 0.055;
     }
 
     if (rgb.b <= 0.0031308) {
         rgb.b *= 12.92;
     } else {
-        rgb.b = 1.055 * pow(rgb.b, 1.0/2.4) - 0.055;
+        rgb.b = 1.055 * pow(rgb.b, 1.0 / 2.4) - 0.055;
     }
 
     return rgb;
 }
 
-bool color_rgb_different(ColorRGB a, ColorRGB b) {
-    if (a.r != b.r || a.g != b.g || a.b != b.b) return true;
-    return false;
+// actual sick (can be interpreted both ways) color science
+#    include "color/cie_1931_2deg_lut.h"
+
+ColorXYZ color_sample_cmf(double wavelength) {
+    if (wavelength < 360.0 || wavelength > 830.0) { return (ColorXYZ) {0}; }
+
+    double idx_f = (wavelength - 360.0);
+    int idx = (int)idx_f;
+    double t = idx_f - (double)idx;
+
+    int idx_next = (idx < 470) ? idx + 1 : idx;
+
+    ColorXYZ final;
+    final.x = lerp(cie_1931_2deg_1nm[idx].x, cie_1931_2deg_1nm[idx_next].x, t);
+    final.y = lerp(cie_1931_2deg_1nm[idx].y, cie_1931_2deg_1nm[idx_next].y, t);
+    final.z = lerp(cie_1931_2deg_1nm[idx].z, cie_1931_2deg_1nm[idx_next].z, t);
+
+    return final;
 }
+
+double color_planck_spectral_exitance(double wavelength, double kelvin) {
+    double lambda = wavelength * 1e-9;
+    double c1 = 2.0 * PI * PLANCK_H * (SPEED_OF_LIGHT * SPEED_OF_LIGHT);
+    double c2 = (PLANCK_H * SPEED_OF_LIGHT) / BOLTZMANN_K;
+    double exponent = c2 / (lambda * kelvin);
+    if (exponent > 700.0) return 0.0;
+    return (c1 / pow(lambda, 5.0)) * (1.0 / (exp(exponent) - 1.0));
+}
+
+ColorXYZ color_kelvin_to_xyz(double kelvin) {
+    ColorXYZ final = {0};
+    for (double l = 360.0; l <= 830.0; l += 1.0) {
+        double spectral_energy = color_planck_spectral_exitance(l, kelvin);
+        ColorXYZ sensitivity = color_sample_cmf(l);
+        final.x += spectral_energy * sensitivity.x;
+        final.y += spectral_energy * sensitivity.y;
+        final.z += spectral_energy * sensitivity.z;
+    }
+
+#    ifndef COLOR_NO_CHROMATICITY_THING_IN_KELVIN_TO_XYZ
+    if (final.y > 0) {
+        double scale = 1.0 / final.y;
+        final.x *= scale;
+        final.y *= scale;
+        final.z *= scale;
+    }
+#    endif
+
+    return final;
+}
+
+static double absorption_coeff(double l) {
+    if (l < 420) return 0.01;
+    if (l < 500) return 0.005;
+    if (l < 600) return 0.1;
+    return 0.6;
+}
+
+ColorXYZ color_cherenkov_to_xyz(double refractive_index) {
+    ColorXYZ final = {0};
+    for (double l = 360.0; l <= 830.0; l += 1.0) {
+        double n_lambda = refractive_index + (0.003 / pow(l / 1000.0, 2.0));
+        double factor = 1.0 - (1.0 / (n_lambda * n_lambda));
+        double spectral_power = factor / (l * l);
+        ColorXYZ sensitivity = color_sample_cmf(l);
+
+#    ifndef COLOR_NO_WATER_ABSORPTION_SIMULATION_IN_CHERENKOV_TO_XYZ
+        double attenuation = exp(-10.0 * absorption_coeff(l));
+        spectral_power *= attenuation;
+#    endif
+
+        final.x += spectral_power * sensitivity.x;
+        final.y += spectral_power * sensitivity.y;
+        final.z += spectral_power * sensitivity.z;
+    }
+
+#    ifndef COLOR_NO_CHROMATICITY_THING_IN_CHERENKOV_TO_XYZ
+    if (final.y > 0) {
+        double scale = 1.0 / final.y;
+        final.x *= scale;
+        final.y *= scale;
+        final.z *= scale;
+    }
+#    endif
+
+    return final;
+}
+
+// tonemapping!! yippee :(
+ColorRGB color_tonemap_lottes(ColorRGB rgb) {
+    const double a = 1.6;
+    const double d = 0.977;
+
+    const double b = 0.0357074191315325;
+    const double c = 1.0152084742456437;
+
+    rgb.r = pow(fmax(0.0, rgb.r), a) / (pow(fmax(0.0, rgb.r), a * d) * c + b);
+    rgb.g = pow(fmax(0.0, rgb.g), a) / (pow(fmax(0.0, rgb.g), a * d) * c + b);
+    rgb.b = pow(fmax(0.0, rgb.b), a) / (pow(fmax(0.0, rgb.b), a * d) * c + b);
+
+    return rgb;
+}
+
 #endif
