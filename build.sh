f32a921d9b74e7c88a5de5d2d470a2f787ebe0e9
Shin Morisawa <git@tryh4rd.dev>
2026-04-26 20:32:54 +0900

    cursed af

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail # thanks cloudflare!!!
 
 if [ ! -d build ]; then
-    AR="llvm-ar" meson setup build -Doptimization=3
+    AR="ar" meson setup build -Doptimization=3
 fi
 
 ninja -C build
